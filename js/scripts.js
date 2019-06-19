e0d9ee5908406e89a06429c874c77b9c8d9726a3
Roger <idevelopedreams@gmail.com>
2019-06-19 09:24:06 -0400

    got up to challenge 5

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,23 @@
 // 1: Set the text of the <h1> element
-
+const myH1 = document.querySelector('h1')
+myH1.textContent = "Bucket List";
 
 // 2: Set the color of the <h1> to a different color
-
+myH1.style.color = "magenta";
 
 // 3: Set the content of the '.desc' paragraph
 // The content should include at least one HTML tag
-
+const desc = document.querySelector('.desc')
+desc.innerHTML = "stuff thats <strong>Fun</strong>"
 
 // 4: Set the class of the <ul> to 'list'
-
+const ulClass = document.querySelector('ul')
+ulClass.setAttribute("class","List")
 
 // 5: Create a new list item and add it to the <ul>
-
+const item = document.createElement('li')
+item.innerHTML = "<input >Surfing"
+ulClass.appendChild(item);
 
 // 6: Change all <input> elements from text fields to checkboxes
 
